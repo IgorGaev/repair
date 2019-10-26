725a02496134e8a371df4d47622bfa7a94992195
Igor Gaev <io.gaev@gmail.com>
2019-10-26 16:20:05 +0500

    Создана и адаптированна секция слайдер методом Slick (19 урок)

---
+++ src/js/slider.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+  $('.portfolio-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.arrow-left'),
+    nextArrow: $('.arrow-right'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 797,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+})
