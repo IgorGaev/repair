35c3b3616a903ccd1d91af338f235084bdd6265e
Igor Gaev <io.gaev@gmail.com>
2019-11-10 21:19:14 +0500

    Добавлен файл .htaccess

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <title>Сайт ремонт квартир</title>
+  <title>Ремонт квартир</title>
 
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
